48d44ca1c9a70bff515503e811cecaa10f1f00b2
Eddie Lau <tatonlto@gmail.com>
2014-08-16 14:05:27 +0800

    initial commit. following http://ibeaconmodules.us tutorial

---
+++ DailyLoyaltyProgram/AppDelegate.swift
@@ -0,0 +1,138 @@
+//
+//  AppDelegate.swift
+//  DailyLoyaltyProgram
+//
+//  Created by Eddie Lau on 16/8/14.
+//  Copyright (c) 2014 42 Labs. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate {
+                            
+    var window: UIWindow?
+    var locationManager: CLLocationManager?
+    var lastProximity: CLProximity?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: NSDictionary?) -> Bool {
+        // Override point for customization after application launch.
+        
+        // Garage Society iBeacon UUID
+        let uuidString = "B9407F30-F5F8-466E-AFF9-25556B57FE6D"
+        let beaconIdentifier = "iBeaconGarageSociety"
+        let beaconUUID:NSUUID = NSUUID(UUIDString: uuidString)
+        let beaconRegion:CLBeaconRegion = CLBeaconRegion(proximityUUID: beaconUUID, identifier: beaconIdentifier)
+        
+        locationManager = CLLocationManager()
+        if(locationManager!.respondsToSelector("requestAlwaysAuthorization")) {
+            locationManager!.requestAlwaysAuthorization()
+        }
+        locationManager!.delegate = self
+        locationManager!.pausesLocationUpdatesAutomatically = false
+        
+        locationManager!.startMonitoringForRegion(beaconRegion)
+        locationManager!.startRangingBeaconsInRegion(beaconRegion)
+        locationManager!.startUpdatingLocation()
+        
+        if(application.respondsToSelector("registerUserNotificationSettings:")) {
+            application.registerUserNotificationSettings(
+                UIUserNotificationSettings(
+                    forTypes: UIUserNotificationType.Alert | UIUserNotificationType.Sound,
+                    categories: nil
+                )
+            )
+        }
+        
+        return true
+    }
+
+    func applicationWillResignActive(application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+}
+
+
+extension AppDelegate: CLLocationManagerDelegate {
+    func sendLocalNotificationWithMessage(message: String!) {
+        let notification:UILocalNotification = UILocalNotification()
+        notification.alertBody = message
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
+    }
+    
+    func locationManager(manager: CLLocationManager!, didRangeBeacons beacons: AnyObject[]!, inRegion region: CLBeaconRegion!) {
+        NSLog("didRangeBeacons");
+    
+        let viewController:ViewController = window!.rootViewController as ViewController
+        viewController.beacons = beacons as CLBeacon[]?
+        viewController.tableView.reloadData()
+        
+        sendLocalNotificationWithRangedMessage(beacons)
+    }
+    
+    func locationManager(manager: CLLocationManager!, didEnterRegion region: CLRegion!) {
+        manager.startRangingBeaconsInRegion(region as CLBeaconRegion)
+        manager.startUpdatingLocation()
+        
+        NSLog("You entered the region")
+        sendLocalNotificationWithMessage("You entered the region")
+    }
+    
+    func locationManager(manager: CLLocationManager!, didExitRegion region: CLRegion!) {
+        manager.stopRangingBeaconsInRegion(region as CLBeaconRegion)
+        manager.stopUpdatingLocation()
+        
+        NSLog("You exited the region")
+        sendLocalNotificationWithMessage("You exited the region")
+    }
+    
+    func sendLocalNotificationWithRangedMessage(beacons: AnyObject[]!) {
+        var message:String = ""
+        
+        if (beacons.count > 0) {
+            let nearestBeacon:CLBeacon = beacons[0] as CLBeacon
+            
+            if(nearestBeacon.proximity == lastProximity || nearestBeacon.proximity == CLProximity.Unknown) {
+                return;
+            }
+            lastProximity = nearestBeacon.proximity;
+            
+            switch nearestBeacon.proximity {
+            case CLProximity.Far:
+                message = "You are far away from the beacon"
+            case CLProximity.Near:
+                message = "You are near the beacon"
+            case CLProximity.Immediate:
+                message = "You are in the immediate proximity of the beacon"
+            case CLProximity.Unknown:
+                return
+            }
+        } else {
+            message = "No beacons are nearby"
+        }
+        
+        NSLog("%@", message)
+        sendLocalNotificationWithMessage(message)
+    }
+}
---
+++ DailyLoyaltyProgramTests/DailyLoyaltyProgramTests.swift
@@ -0,0 +1,35 @@
+//
+//  DailyLoyaltyProgramTests.swift
+//  DailyLoyaltyProgramTests
+//
+//  Created by Eddie Lau on 16/8/14.
+//  Copyright (c) 2014 42 Labs. All rights reserved.
+//
+
+import XCTest
+
+class DailyLoyaltyProgramTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
